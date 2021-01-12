1e045ed826c64c163243bb0d275f392f3e5addd4
AralinIvan <Privet129>
2021-01-12 20:49:54 +0300

    1

---
+++ board.py
@@ -0,0 +1,287 @@
+class Game_error(Exception):
+    """Errors related to the game in general"""
+    pass
+
+class Illegal_move(Game_error):
+    """Errors from illegal moves"""
+    pass
+
+class Game_rule_error(Game_error):
+    """Errors that arise from rule issues"""
+    pass
+
+
+class Board(object):
+    def __init__(self):
+        super(Board, self).__init__()
+        self.turn = 1
+        self.a = ''
+        self.player = 2
+        self.victory = 0
+        self.board = [[0 for x in range(8)] for x in range(8)]
+        self.board[3][3] = 1
+        self.board[3][4] = 2
+        self.board[4][3] = 2
+        self.board[4][4] = 1
+        self.has_changed = True
+
+
+
+    def player_move(self, x, y):
+        if self.victory != 0:
+            return
+        self.perform_move(x, y)
+
+    def perform_move(self, x, y):
+        if self.board[x][y] != 0:
+            raise Illegal_move("Player {0} tried to place a tile at {1},{2} but it is already occupied by {3}".format(
+                self.player,
+                x, y,
+                self.board[x][y]
+            ))
+
+        self.place_piece(x, y)
+        all_tiles = [item for sublist in self.board for item in sublist]
+        empty_tiles = sum(1 for tile in all_tiles if tile == 0)
+        white_tiles = sum(1 for tile in all_tiles if tile == 1)
+        black_tiles = sum(1 for tile in all_tiles if tile == 2)
+        if white_tiles < 1 or black_tiles < 1 or empty_tiles < 1:
+            self.end_game()
+            return
+        self.player = 3 - self.player
+        move_found = self.move_can_be_made()
+        if not move_found:
+            self.a = 'игрок ' + str(self.player) + ' не имеет возможных ходов ход переходит игроку ' + str(3 - self.player)
+            self.player = 3 - self.player
+        self.has_changed = True
+
+    def move_can_be_made(self):
+        move_found = False
+
+        for x in range(0, 8):
+            for y in range(0, 8):
+                if move_found:
+                    continue
+                if self.board[x][y] == 0:
+                    c = self.place_piece(x, y, live_mode=False)
+                    if c > 0:
+                        move_found = True
+
+        return move_found
+
+    def place_piece(self, x, y, live_mode=True):
+        if live_mode:
+            self.board[x][y] = self.player
+        change_count = 0
+        column = self.board[x]
+        row = [self.board[i][y] for i in range(0, 8)]
+        if self.player in column[:y]:
+            changes = []
+            search_complete = False
+            for i in range(y - 1, -1, -1):
+                if search_complete:
+                    continue
+                counter = column[i]
+                if counter == 0:
+                    changes = []
+                    search_complete = True
+                elif counter == self.player:
+                    search_complete = True
+                else:
+                    changes.append(i)
+            if search_complete:
+                change_count += len(changes)
+                if live_mode:
+                    for i in changes:
+                        self.board[x][i] = self.player
+
+        if self.player in column[y:]:
+            changes = []
+            search_complete = False
+            for i in range(y + 1, 8, 1):
+                if search_complete:
+                    continue
+                counter = column[i]
+                if counter == 0:
+                    changes = []
+                    search_complete = True
+                elif counter == self.player:
+                    search_complete = True
+                else:
+                    changes.append(i)
+            if search_complete:
+                change_count += len(changes)
+                if live_mode:
+                    for i in changes:
+                        self.board[x][i] = self.player
+
+        if self.player in row[:x]:
+            changes = []
+            search_complete = False
+            for i in range(x - 1, -1, -1):
+                if search_complete: continue
+                counter = row[i]
+                if counter == 0:
+                    changes = []
+                    search_complete = True
+                elif counter == self.player:
+                    search_complete = True
+                else:
+                    changes.append(i)
+            if search_complete:
+                change_count += len(changes)
+                if live_mode:
+                    for i in changes:
+                        self.board[i][y] = self.player
+
+        if self.player in row[x:]:
+            changes = []
+            search_complete = False
+            for i in range(x + 1, 8, 1):
+                if search_complete: continue
+                counter = row[i]
+                if counter == 0:
+                    changes = []
+                    search_complete = True
+                elif counter == self.player:
+                    search_complete = True
+                else:
+                    changes.append(i)
+            if search_complete:
+                change_count += len(changes)
+                if live_mode:
+                    for i in changes:
+                        self.board[i][y] = self.player
+
+        i, j = x - 7, y + 7
+        bl_tr_diagonal = []
+        for q in range(0, 16):
+            if 0 <= i < 8 and 0 <= j < 8:
+                bl_tr_diagonal.append(self.board[i][j])
+
+            i += 1
+            j -= 1
+        i, j = x - 7, y - 7
+        br_tl_diagonal = []
+        for q in range(0, 16):
+            if 0 <= i < 8 and 0 <= j < 8:
+                br_tl_diagonal.append(self.board[i][j])
+            i += 1
+            j += 1
+        if self.player in bl_tr_diagonal:
+            changes = []
+            search_complete = False
+            i = 0
+            lx, ly = x, y
+            while 0 <= lx < 8 and 0 <= ly < 8:
+                lx += 1
+                ly -= 1
+                if lx > 7 or ly < 0: break
+                if search_complete: continue
+                counter = self.board[lx][ly]
+                if counter == 0:
+                    changes = []
+                    search_complete = True
+                elif counter == self.player:
+                    search_complete = True
+                else:
+                    changes.append((lx, ly))
+            if search_complete:
+                change_count += len(changes)
+                if live_mode:
+                    for i, j in changes:
+                        self.board[i][j] = self.player
+        if self.player in bl_tr_diagonal:
+            changes = []
+            search_complete = False
+            i = 0
+            lx, ly = x, y
+            while 0 <= lx < 8 and 0 <= ly < 8:
+                lx -= 1
+                ly += 1
+                if lx < 0 or ly > 7: break
+                if search_complete: continue
+                counter = self.board[lx][ly]
+                if counter == 0:
+                    changes = []
+                    search_complete = True
+                    break
+                elif counter == self.player:
+                    search_complete = True
+                    break
+                else:
+                    changes.append((lx, ly))
+            if search_complete:
+                change_count += len(changes)
+                if live_mode:
+                    for i, j in changes:
+                        self.board[i][j] = self.player
+        if self.player in br_tl_diagonal:
+            changes = []
+            search_complete = False
+            i = 0
+            lx, ly = x, y
+            while 0 <= lx < 8 and 0 <= ly < 8:
+                lx -= 1
+                ly -= 1
+                if lx < 0 or ly < 0: break
+                if search_complete: continue
+                counter = self.board[lx][ly]
+                if counter == 0:
+                    changes = []
+                    search_complete = True
+                elif counter == self.player:
+                    search_complete = True
+                else:
+                    changes.append((lx, ly))
+            if search_complete:
+                change_count += len(changes)
+                if live_mode:
+                    for i, j in changes:
+                        self.board[i][j] = self.player
+
+        if self.player in br_tl_diagonal:
+            changes = []
+            search_complete = False
+            i = 0
+            lx, ly = x, y
+            while 0 <= lx < 8 and 0 <= ly < 8:
+                lx += 1
+                ly += 1
+                if lx > 7 or ly > 7: break
+                if search_complete: continue
+                counter = self.board[lx][ly]
+                if counter == 0:
+                    changes = []
+                    search_complete = True
+                elif counter == self.player:
+                    search_complete = True
+                else:
+                    changes.append((lx, ly))
+            if search_complete:
+                change_count += len(changes)
+                if live_mode:
+                    for i, j in changes:
+                        self.board[i][j] = self.player
+        if change_count == 0 and live_mode:
+            self.board[x][y] = 0
+            raise Illegal_move("Player {0} tried to place a tile at {1},{2} but that will result in 0 flips".format(
+                self.player,
+                x, y,
+            ))
+        return change_count
+
+    def end_game(self):
+        all_tiles = [item for sublist in self.board for item in sublist]
+
+        white_tiles = sum(1 for tile in all_tiles if tile == 1)
+        black_tiles = sum(1 for tile in all_tiles if tile == 2)
+
+        if white_tiles > black_tiles:
+            self.victory = 1
+        elif white_tiles < black_tiles:
+            self.victory = 2
+        else:
+            self.victory = -1
+
+        self.has_changed = True
--- main.py
+++ main.py
@@ -1,11 +1,13 @@
 import pygame, sys
+import board
 
 
 size = WIDTH, HEIGHT = 800, 800
 tile_size = 100
-offset_size = 5
+offset_size = 10
+FPS = 50
 
-def terminate(self):
+def terminate():
     pygame.quit()
     sys.exit()
 
@@ -14,21 +16,36 @@ class Eng(object):
     def __init__(self):
         super(Eng, self).__init__()
         pygame.init()
+        self.font = pygame.font.SysFont('arial', 48)
         self.textures = {}
-        self.clock = pygame.time.clock
+        self.clock = pygame.time.Clock()
         self.screen = pygame.display.set_mode(size)
         pygame.display.set_caption('Реверси')
         self.textures['board'] = pygame.image.load('data/board.png')
         self.textures['black'] = pygame.image.load('data/black.png')
         self.textures['white'] = pygame.image.load('data/white.png')
-        self.game = reversi.Reversi()
+        self.game = board.Board()
         self.draw_board()
+
+    def drawText(self, text, surface, x, y):
+        textobject = self.font.render(text, 1, (0, 0, 0))
+        textrect = textobject.get_rect()
+        textrect.topleft = (x, y)
+        surface.blit(textobject, textrect)
+
 
     def draw_board(self):
         the_board = pygame.Rect(0, 0, WIDTH, HEIGHT)
         self.screen.blit(self.textures['board'], the_board)
+        if self.game.a != '':
+            self.drawText(self.game.a, self.screen, 68, 10)
+        else:
+            if self.game.player == 2:
+                self.drawText('ход чёрных', self.screen, 39, 40)
+            else:
+                self.drawText('ход белых', self.screen, 39, 40)
         for i in range(0, 8):
-            for j on range(0, 8):
+            for j in range(0, 8):
                 player = self.game.board[i][j]
                 counter = pygame.Rect(i * tile_size + offset_size, j * tile_size + offset_size,
                                       tile_size - offset_size * 2, tile_size - offset_size * 2)
@@ -41,17 +58,17 @@ class Eng(object):
                     self.drawText('Победа белых', self.screen, 38, 10)
                 elif self.game.victory == 2:
                     self.drawText('Победа чёрных', self.screen, 39, 10)
-                else:
+                elif self.game.victory == -1:
                     self.drawText('Ничья', self.screen, 38, 10)
                 pygame.display.update()
 
     def mouse_click(self, event):
-        x,y = event.pos
+        x, y = event.pos
         tile_x = int(x // tile_size)
         tile_y = int(y // tile_size)
         try:
-            self.game.player_move(tx, ty)
-        except reversi.Illegal_move as e:
+            self.game.player_move(tile_x, tile_y)
+        except board.Illegal_move as e:
             print("Illegal move")
         except Exception as e:
             raise
@@ -60,8 +77,6 @@ class Eng(object):
     def start(self):
         self.__init__()
         self.game.__init__()
-
-
         while True:
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
@@ -69,6 +84,18 @@ class Eng(object):
 
                 elif event.type == pygame.MOUSEBUTTONDOWN:
                     self.mouse_click(event)
+
+            if self.game.has_changed:
+                self.draw_board()
+                self.game.has_changed = False
+                self.game.a = ''
+            self.clock.tick()
+
+
+if __name__ == '__main__':
+    eng = Eng()
+    eng.start()
+
 
 
 
